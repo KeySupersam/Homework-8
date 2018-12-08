a7f369ebe87091a0c74bada73153e61641512f83
Sarah I <45565606+KeySupersam@users.noreply.github.com>
2018-12-08 15:08:11 -0600

    Add files via upload

---
+++ hmwk8.py
@@ -0,0 +1,138 @@
+#8.1
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+display_message()
+#8.2
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('To Kill a Mockingbird')
+#8.4
+def make_shirt(size='large', message='I love Python!'):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
+#8.6
+def city_country(city, country):
+    """Return a string like 'Santiago, Chile'."""
+    return(city.title() + ", " + country.title())
+
+city = city_country('santiago', 'chile')
+print(city)
+
+city = city_country('london', 'england')
+print(city)
+
+city = city_country('new york city', 'united states')
+print(city)
+#8.8
+def make_album(artist, title, tracks=0):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+    }
+    if tracks:
+        album_dict['tracks'] = tracks
+    return album_dict
+
+title_prompt = "\nWhat album are you thinking of? "
+artist_prompt = "Who's the artist? "
+
+print("Enter 'quit' at any time to stop.")
+
+while True:
+    title = input(title_prompt)
+    if title == 'quit':
+        break
+
+    artist = input(artist_prompt)
+    if artist == 'quit':
+        break
+
+    album = make_album(artist, title)
+    print(album)
+
+print("\nThanks for responding!")
+#8.10
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    great_magicians = []
+
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\n")
+make_great(magicians)
+show_magicians(magicians)
+#8.12
+def make_sandwich(*items):
+    """Make a sandwich with the given items."""
+    print("\nI'll make you a great sandwich:")
+    for item in items:
+        print("  ...adding " + item + " to your sandwich.")
+    print("Your sandwich is ready!")
+
+make_sandwich('roast beef', 'american cheese', 'lettuce', 'honey')
+make_sandwich('turkey', 'apple slices', 'honey mustard')
+make_sandwich('peanut butter', 'grape jam')
+#8.14
+def make_car(manufacturer, model, **options):
+    """Make a dictionary representing a car."""
+    car_dict = {
+        'manufacturer': manufacturer.title(),
+        'model': model.title(),
+        }
+    for option, value in options.items():
+        car_dict[option] = value
+
+    return car_dict
+
+my_outback = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(my_outback)
+
+my_accord = make_car('honda', 'accord', year=1991, color='white',
+        headlights='popup')
+print(my_accord)
+#8.15
+"""Functions related to printing 3d models."""
+
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until there are none left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
